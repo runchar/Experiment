b553666bdbf998c348e29abc8d91de88f86d6d31
runchar <3334558156@qq.com>
2024-04-04 15:05:49 +0800

    basic function implemented

--- bitstream.h
+++ bitstream.h
@@ -2,26 +2,25 @@
 #include <cstring>
 #endif 
 
-namespace BitStream{
+namespace BitSet{
     using storage_type = size_t;
     using storage_pointer = storage_type *;
     using ULL = unsigned long long; 
 
     class bits{
-
-    public:
+protected :
     const ULL length=sizeof(storage_type)*8;  //length of storage_type in bits
-    ULL SizeOfType=0;  //size of bitset in terms of storage_type
-    size_t SizeOfBit;
+    ULL SizeOfType=0;  //size of storage_type
+    size_t SizeOfBit;  //size of bitset in bits// uesd to judge if beyond
     storage_pointer bitset=nullptr;
-
+public:
         bits(){
             SizeOfBit=0;
             SizeOfType=1;
             bitset = new storage_type[SizeOfType];
             memset(bitset,0,SizeOfType*sizeof(storage_type));
          }
-
+        //fault constructor
         bits( ULL n ){
             SizeOfBit=sizeof(ULL)*8;
             SizeOfType=(SizeOfBit-1)/length+1;
@@ -55,13 +54,14 @@ namespace BitStream{
                 }
             }
         }
-
+        //ULL constructor
         bits( const char* s , size_t n , char zero , char one ){
             // SizeOfBit=((n-1)/length+1)*length;
             SizeOfBit=n;//special because string length may be not multiple of 64
             SizeOfType=(SizeOfBit-1)/length+1;
             bitset = new storage_type[SizeOfType];
             memset(bitset,0,SizeOfType*sizeof(storage_type));
+            size_t BitsCnt=0;
             for(ULL i=0;i<SizeOfType;i++)
             {
                 for(ULL j=0;j<length;j++)
@@ -77,10 +77,122 @@ namespace BitStream{
                         bitset[i]=bitset[i] | (1ull<<j);
                     }
                     else
+                    {
                         throw "invalid_arguments";//????
+                    }
+                        BitsCnt++;;
                 }
-
+                
             }
+            if(BitsCnt!=n)
+                throw "wrong_length";
+        }
+        //string constructor
+
+        ~bits(){
+            delete[] bitset;
+        }   
+
+        bool test(size_t p){
+            if(p>=SizeOfBit)
+                throw "beyond";
+            else
+                return (bitset[p/length] & (1ull<<(p%length)));
+        }
+
+        bool operator[](size_t p){
+            if(p>=SizeOfBit)
+                throw "beyond";
+            else
+                return (bitset[p/length] & (1ull<<(p%length)));
+        }
+        //impletment by overload [] , same with `test`
+
+        size_t count(){
+            size_t cnt=0;
+            for(ULL i=0;i<SizeOfBit;i++)
+            {
+                // if(bitset[i/length] & (1ull<<(i%length)))
+                //     cnt++;
+                if(this->operator[](i)) //another way to implement by using `this` pointer
+                    cnt++;              //`this` point to the object itself
+            }
+            return cnt;
+        }
+
+        size_t size(){
+            return sizeof(*this)+SizeOfType*sizeof(storage_type);
+        }
+
+        bool all(){
+            for(ULL i=0;i<SizeOfBit;i++)
+            {
+                if(this->operator[](i)==0)
+                    return false;
+            }
+            return true;
+        }
+       
+        bool any(){
+            return !none();
+        }
+
+        bool none(){
+            for(ULL i=0;i<SizeOfBit;i++)
+            {
+                if(this->operator[](i)==1)
+                    return false;
+            }
+            return true;
+        }
+
+        bits& filp(){
+            for(ULL i=0;i<SizeOfType;i++)
+            {
+                bitset[i]=~bitset[i];
+            }
+            return *this;
+        }
+
+        bits& filp(size_t p)
+        {
+            if(p>=SizeOfBit)
+                throw "beyond";
+            else
+            {
+                if(this->operator[](p))
+                    bitset[p/length]=bitset[p/length] & ~(1ull<<(p%length));//set 0
+                else
+                    bitset[p/length]=bitset[p/length] | (1ull<<(p%length));//set 1
+            }
+            return *this;
+        }
+
+        bits& set(){
+            for(ULL i=0;i<SizeOfType;i++)
+            {
+                bitset[i]=~0;
+            }
+            return *this;
+        }
+
+        bits& reset(){
+            for(ULL i=0;i<SizeOfType;i++)
+            {
+                bitset[i]=0;
+            }
+            return *this;
+        }
+        
+        bool compare(const bits& y){
+            if(SizeOfBit!=y.SizeOfBit||SizeOfType!=y.SizeOfType)
+                throw "different_size";
+            for(ULL i=0;i<SizeOfType;i++)
+            {
+                if(bitset[i]!=y.bitset[i])
+                    return false;
+            }
+            return true;
         }
 
         void print(){
@@ -96,7 +208,9 @@ namespace BitStream{
                         std::cout<<0;
                 }
             }
+            std::cout<<std::endl;
         }
+        //check 
     };
 }
 
--- main.cpp
+++ main.cpp
@@ -3,17 +3,36 @@
 
 int main(){
     try {
-        char aa[]="1111000";
-        BitStream::bits a(aa,7,'0','1');
-        BitStream::bits b(127);
-        BitStream::bits c;
-        int i=1;
-        i++;
-        a.print();
-        std::cout<<std::endl;
+        char aa[]="1011010110101010101010101000001111101010010010101100101111111110111";
+        char bb[]="111111111";
+
+        BitSet::bits a(aa,67,'0','1');
+        BitSet::bits b(bb,9,'0','1');
+        BitSet::bits c;
+        // a.print();
+        // std::cout<<std::endl;
+        // for(int i=0;i<=5;i++)
+        //     std::cout<<a[i]; 
+        // std::cout<<std::endl;
+        // std::cout<<a.count()<<std::endl;
+        // std::cout<<a.size()<<std::endl;
+        // std::cout<<b.size()<<std::endl;
+        // b.print();
+        // std::cout<<std::endl;
+        // c.print();
+        // std::cout<<a.all()<<std::endl;
+        // std::cout<<a.any()<<std::endl;
+        // std::cout<<a.none()<<std::endl;
+        // std::cout<<b.all()<<std::endl;
+        // std::cout<<b.any()<<std::endl;
+        // std::cout<<b.none()<<std::endl;
+        auto temp=b.filp(3);
         b.print();
-        std::cout<<std::endl;
-        c.print();
+        auto temp2=temp.set();
+        b.print();
+        auto temp3=temp2.reset();
+        b.print();
+        std::cout<<temp3.compare(b)<<std::endl;
     } catch (const char* msg) {
         std::cerr << "Exception caught: " << msg << std::endl;
     }
